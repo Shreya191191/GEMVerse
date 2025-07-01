17f5003b7302dbf608012c35d0d2f0db37a38d71
Shreya191191 <shreyatripathi@gmail.com>
2025-07-01 19:30:06 +0530

    Fixed drawable issue

--- app/src/main/java/eu/tutorials/gemverse/ChatPage.kt
+++ app/src/main/java/eu/tutorials/gemverse/ChatPage.kt
@@ -2,7 +2,16 @@ package eu.tutorials.gemverse
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -14,25 +23,26 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import eu.tutorials.GEMVerse.R
 import eu.tutorials.gemverse.ui.theme.ColorModalMessage
 import eu.tutorials.gemverse.ui.theme.ColorUserMessage
-
-import androidx.compose.ui.platform.LocalSoftwareKeyboardController
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.ExperimentalComposeUiApi
 import kotlinx.coroutines.delay
+
 
 
 @Composable
@@ -66,7 +76,7 @@ fun MessageList(modifier: Modifier = Modifier, messageList: List<MessageModel>) 
             verticalArrangement = Arrangement.Center
         ) {
             Image(
-                painter = painterResource(id = R.drawable.ic_launcher_firstpage),
+                painter = painterResource(id= R.drawable.ic_google),
                 contentDescription = "Icon",
                 modifier = Modifier
                     .size(200.dp)
